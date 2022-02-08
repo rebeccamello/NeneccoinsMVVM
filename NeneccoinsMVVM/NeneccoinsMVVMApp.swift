8e5d50f85b8974cdc8b8b4348bacfafabdbc078d
Rebecca Mello <rebecca.mello@me.com>
2022-02-08 16:53:54 -0300

    Initial commit

---
+++ NeneccoinsMVVM/NeneccoinsMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeneccoinsMVVMApp.swift
+//  NeneccoinsMVVM
+//
+//  Created by Rebecca Mello on 08/02/22.
+//
+
+import SwiftUI
+
+@main
+struct NeneccoinsMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
